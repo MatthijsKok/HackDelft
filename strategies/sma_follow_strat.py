3c569c3002eb98fb78ee1daa72d1dca1d78d0777
Matthijs Kok <matthijs.kok@me.com>
2017-05-14 01:44:53 +0200

    Update position list seperately from order

--- strategies/sma_follow_strat.py
+++ strategies/sma_follow_strat.py
@@ -1,5 +1,5 @@
 import numpy as np
-import simple_moving_average as sma
+from simple_moving_average import simple_moving_average as sma
 
 
 class Strategy:
@@ -19,41 +19,30 @@ class Strategy:
         # print(self.position_list)
 
     def order(self, time, order_amount):
-        # print()
-        # print('ORDERING...')
-        # print('at time: {} wanted order of {}: '.format(time, order_amount))
-        current_position = self.position_list[time-1]
-        # print('current position: {}'.format(current_position))
+        current_position = self.position_list[time]
         desired_position = current_position + order_amount
-        # print('desired position: {}'.format(desired_position))
         if desired_position < -100:
             actual_order_amount = self.min_position - current_position
         elif desired_position > 100:
             actual_order_amount = self.max_position - current_position
         else:
             actual_order_amount = order_amount
-        # print('actual order amount: {}'.format(actual_order_amount))
         self.trades_list[time] = actual_order_amount
-        self.position_list[time] = current_position + actual_order_amount
-
-        # print('FINISHED ORDER!!!')
-        # print()
 
     def order_until_at_position(self, time, position):
-        # print('ordering until at position: {}'.format(position))
-        # print('current position: {}'.format(self.position_list[time]))
-
         current_position = self.position_list[time]
         self.order(time, (position - current_position))
 
-        # print('new position: {}'.format(self.position_list[time]))
-        # print()
+    def update_position(self, time):
+        self.position_list[time] = self.position_list[time-1] + self.trades_list[time-1]
 
     def implement_strategy(self):
-        sma10_list = sma.simple_moving_average(self.prices_list, 10)
-        sma200_list = sma.simple_moving_average(self.prices_list, 200)
+        sma10_list = sma(self.prices_list, 50)
+        sma200_list = sma(self.prices_list, 500)
         for i, price in enumerate(self.prices_list):
-            if price > sma200_list[i]:
+            self.update_position(i)
+
+            if price < sma200_list[i]:
                 if price > sma10_list[i]:
                     self.order_until_at_position(i, 0)
                 else:
@@ -63,3 +52,4 @@ class Strategy:
                     self.order_until_at_position(i, -100)
                 else:
                     self.order_until_at_position(i, 0)
+        self.order_until_at_position(len(self.prices_list)-1, 0)
