c3139400c5d59dc34f181c1865bcf7030cf0c231
Timo van Asten <Timo@wlan-145-94-160-131.wlan.tudelft.nl>
2017-05-13 23:29:48 +0200

    Show plot for different averages

--- plot_all_data.py
+++ plot_all_data.py
@@ -1,15 +1,21 @@
 import matplotlib.pyplot as plt
 from reader import read_all
+import numpy as np
 
-
+# Read in prices files
 prices = read_all()
 
+# Get the prices from the file
 prices_list = prices.price
-print(prices_list)
 
-# fig, ax = plt.subplots()
-# plt.plot(ax=ax, title='Prices')
-# plt.show()
+# Trend lines for different averages
+trends = [1, 10, 300]
 
-plt.plot(prices_list)
+# Calculate trend lines
+for trend in trends:
+    trend_line = np.convolve(prices_list, np.ones(trend) / trend, mode='full')
+    plt.plot(trend_line)
+
+# Show plot
+plt.legend(trends, loc='lower center');
 plt.show()
