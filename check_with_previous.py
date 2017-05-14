1c293ea2e8cc88053f8c8ca3e17f3f3b36c2d5b0
Jurriaan Den Toonder <speedy4104@hotmail.com>
2017-05-14 10:40:06 +0200

    Finished check_with_previous

--- check_with_previous.py
+++ check_with_previous.py
@@ -1,13 +1,12 @@
 import numpy as np
-import logging
 
 
 def check_with_previous(the_list, window):
-    amount = 0
-    checked = 0
     compare_list = np.zeros(len(the_list))
-    for i in range(0, len(the_list)):
-        for j in range(1, window):
+    for i in range(window, len(the_list)):
+        amount = 0
+        checked = 0
+        for j in range(1, window + 1):
             if the_list[i - j] < the_list[i]:
                 amount = amount - 1
             elif the_list[i - j] > the_list[i]:
@@ -15,6 +14,3 @@ def check_with_previous(the_list, window):
             checked = checked + 1
         compare_list[i] = amount / checked
     return compare_list
-
-some_list = [4, 10, 2, 20, 15, 3, 3]
-logging.debug(check_with_previous(some_list, 1))
