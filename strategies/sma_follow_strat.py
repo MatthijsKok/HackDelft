66b348dee7165f08d8f8bab2afdfce471b80a20a
Matthijs Kok <matthijs.kok@me.com>
2017-05-14 11:31:07 +0200

    update sma_follow_strat and time_machine. only keep previous position instead of list.

--- strategies/sma_follow_strat.py
+++ strategies/sma_follow_strat.py
@@ -7,48 +7,47 @@ class Strategy:
     def __init__(self, price_list, max_position=100):
         self.prices_list = price_list
         self.trades_list = np.zeros(len(price_list))
-        self.position_list = np.zeros(len(price_list))
-        self.max_position = max_position
-        self.min_position = -max_position
+        self.max_pos = max_position
+        self.min_pos = -max_position
+
+        self.current_time = 0
+        self.current_pos = 0
 
         # Execute strategy
         self.implement_strategy()
 
-    def order(self, time, order_amount):
-        current_position = self.position_list[time]
-        desired_position = current_position + order_amount
-        if desired_position < -100:
-            actual_order_amount = self.min_position - current_position
-        elif desired_position > 100:
-            actual_order_amount = self.max_position - current_position
-        else:
-            actual_order_amount = order_amount
-        self.trades_list[time] = actual_order_amount
+    def order(self, order_amount):
+        # Calculate order amount
+        actual_order_amount = np.clip(order_amount, self.min_pos - self.current_pos, self.max_pos - self.current_pos)
 
-    def order_until_at_position(self, time, position):
-        current_position = self.position_list[time]
-        self.order(time, (position - current_position))
+        # Execute the order
+        self.trades_list[self.current_time] = actual_order_amount
 
-    def update_position(self, time):
-        self.position_list[time] = self.position_list[time-1] + self.trades_list[time-1]
+    def order_until_at_position(self, position):
+        self.order(position - self.current_pos)
+
+    def update_position(self):
+        self.current_pos = self.current_pos + self.trades_list[self.current_time]
 
     def implement_strategy(self):
         small_sma = sma(self.prices_list, 5)
         big_sma = sma(self.prices_list, 50)
+        last_second = len(self.prices_list) - 1
         for i, price in enumerate(self.prices_list):
-            self.update_position(i)
-
-            if price < big_sma[i]:
-                if price < small_sma[i]:
-                    self.order_until_at_position(i, 100)
-                elif self.position_list[i] > 0:
-                    self.order_until_at_position(i, 0)
-            else:
-                if price > small_sma[i]:
-                    self.order_until_at_position(i, -100)
-                elif self.position_list[i] < 0:
-                    self.order_until_at_position(i, 0)
-
+            self.current_time = i
             # Reset your position at the end of the day
-            if i == len(self.prices_list)-1:
-                self.order_until_at_position(len(self.prices_list)-1, 0)
+            if self.current_time == last_second:
+                self.order_until_at_position(0)
+            else:
+                if price < big_sma[i]:
+                    if price < small_sma[i]:
+                        self.order_until_at_position(100)
+                    elif self.current_pos > 0:
+                        self.order_until_at_position(0)
+                else:
+                    if price > small_sma[i]:
+                        self.order_until_at_position(-100)
+                    elif self.current_pos < 0:
+                        self.order_until_at_position(0)
+
+            self.update_position()
